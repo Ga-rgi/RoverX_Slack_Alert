edb15e92ea3f026be3fa554631e771dc047eb65d
Gargi <126082566+Ga-rgi@users.noreply.github.com>
2023-03-07 11:58:50 +0530

    Update main.go

--- main.go
+++ main.go
@@ -1,3 +1,8 @@
+
+/* Two programs effectively execute the requirement to send Slack notification.
+slacknotifs/TriggerNotification creates a task in the task queue 
+slacknotifs/ acknowledge_task gets the user address, checks if its whitelisted and sends the slack alert */
+
 package main
 
 import (
@@ -7,14 +12,16 @@ import (
 	"net/http"
 )
 
+//sends a user wallet address to TriggerNotification (performed by wallet_service as a part of the user sign up workflow)
 func Handler_task(w http.ResponseWriter, r *http.Request) {
 	user_wallet_address := "0x1234567890123456789012345678901234567890"
-
+	
 	task_name := slacknotif.TriggerNotification(user_wallet_address)
 
 	fmt.Printf("Create Task : %s \n ", task_name)
 }
 
+// Gets user wallet from the Task queue
 func acknowledgeTaskHandler(w http.ResponseWriter, r *http.Request) {
 	ctx := r.Context()
 
@@ -23,7 +30,8 @@ func acknowledgeTaskHandler(w http.ResponseWriter, r *http.Request) {
 		http.Error(w, "Failed to read request body", http.StatusBadRequest)
 		return
 	}
-
+	
+        //calls Acknowledgetask for the slack alert
 	slacknotif.AcknowledgeTask(ctx, reqBody)
 
 	w.WriteHeader(http.StatusOK)
@@ -32,7 +40,7 @@ func acknowledgeTaskHandler(w http.ResponseWriter, r *http.Request) {
 
 func main() {
 
-	http.HandleFunc("/app/v1/in_house/task_trigger", Handler_task)
+	http.HandleFunc("/app/v1/in_house/task_trigger", Handler_task) //handles route to TriggerNotification
 	http.HandleFunc("/app/v1/in_house/acknowledge_task", acknowledgeTaskHandler)
 	http.ListenAndServe(":8080", nil)
 }
